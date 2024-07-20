7e045f887287304ad45347439a6575917858544f
Denis <capelete.denis@hotmail.com>
2024-07-20 15:27:23 -0300

    iniciando o modal de confirmar viagem

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { MapPin, Calendar, ArrowRight, UserRoundPlus, Settings2, X, AtSign, Plus } from 'lucide-react'
+import { MapPin, Calendar, ArrowRight, UserRoundPlus, Settings2, X, AtSign, Plus, User } from 'lucide-react'
 import { useState, type FormEvent } from 'react';
 
 export function App() {
@@ -7,9 +7,9 @@ export function App() {
 
   const [isGuestsModalOpen, setIsGuestsModalOpen] = useState(false)
 
-  const [emailsToInvite, setEmailsToInvite] = useState([
-    'jessica.teste@yahoo.com'
-  ])
+  const [isConfirmTripModalOpen, setIsConfrimTripModalOpen] = useState(false)
+
+  const [emailsToInvite, setEmailsToInvite] = useState([])
 
   function openGuestsInput() {
     setIsGuestsInputOpen(true);
@@ -25,6 +25,14 @@ export function App() {
 
   function closeGuestsModal() {
     setIsGuestsModalOpen(false)
+  }
+
+  function openConfirmTripModal() {
+    setIsConfrimTripModalOpen(true)
+  }
+
+  function closeConfirmTripModal() {
+    setIsConfrimTripModalOpen(false)
   }
 
   function addNewEmailToInvite(event: FormEvent<HTMLFormElement>) {
@@ -100,7 +108,7 @@ export function App() {
 
               <div className='w-px h-6 bg-zinc-800' />
 
-              <button className='bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400 '>
+              <button onClick={openConfirmTripModal} className='bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400 '>
                 Confirmar viagem
                 <ArrowRight className='size-5 text-lime-950' />
               </button>
@@ -164,6 +172,53 @@ export function App() {
       )
       }
 
+      {isConfirmTripModalOpen && (
+
+        <div className='fixed inset-0 bg-black/60 flex items-center justify-center'>
+          <div className='w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5'>
+            <div className='space-y-2'>
+              <div className='flex items-center justify-between'>
+                <h2 className='text-lg font-semibold'>Confirmar criação de viagem</h2>
+                <button>
+                  <X onClick={closeConfirmTripModal} className='size-5 text-zinc-400' />
+                </button>
+              </div>
+              <p className='text-sm text-zinc-400'>
+                Para concluir a criação da viagem para <span className='font-semibold text-zinc-100'>Florianópolis, Brasil</span> nas datas de <span className='font-semibold text-zinc-100'>16 a 27 de Agosto de 2024</span> preencha seus dados abaixo:
+              </p>
+            </div>
+
+            <div className='flex flex-wrap gap-2'>
+
+            </div>
+            <form onSubmit={addNewEmailToInvite} className='space-y-3'>
+              <div className='h-14 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2'>
+                <User className='text-zinc-400 size-5' />
+                <input
+                  name='name'
+                  placeholder="Seu nome completo?"
+                  className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1" />
+              </div>
+
+              <div className='h-14 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2'>
+                <User className='text-zinc-400 size-5' />
+                <input
+                  name='email'
+                  type="email"
+                  placeholder="Seu e-mail pessoal?"
+                  className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1" />
+              </div>
+
+              <button type='submit' className='bg-lime-300 w-full justify-center text-lime-950 rounded-lg px-5 h-11 font-medium flex items-center gap-2 hover:bg-lime-400 '>
+                Confirmar criação da viagem
+              </button>
+
+            </form>
+
+          </div>
+        </div>
+
+      )}
 
     </div >
   )
